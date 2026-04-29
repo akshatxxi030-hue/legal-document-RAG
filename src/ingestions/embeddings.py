a774f26e3d5331deea6cd4aa80197b973ba6dd5b
Akshat Singh <akshatxxi030@gmail.com>
2026-04-29 23:45:25 +0530

    Added dockerfile

--- src/ingestions/embeddings.py
+++ src/ingestions/embeddings.py
@@ -1,7 +1,7 @@
 from langchain_openai import OpenAIEmbeddings
 
 def get_embeddings():
-    embeddings=OpenAIEmbeddings(
-        model_name="text-embedding-3-small"
+    return OpenAIEmbeddings(
+        model="text-embedding-3-small"
     )
-    return embeddings
+   
